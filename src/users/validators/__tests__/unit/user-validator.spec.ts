aa9482dcd350425cb2d837ce663626ea265be754
akihito7 <akihitopro7@gmail.com>
2025-04-07 09:15:07 -0300

    feat : added tests to field createdAt

--- src/users/validators/__tests__/unit/user-validator.spec.ts
+++ src/users/validators/__tests__/unit/user-validator.spec.ts
@@ -106,4 +106,61 @@ describe('UserValidator Unit Tests', () => {
     });
 
   })
+
+  describe('Passsword field validation', () => {
+
+    it('should return all validation errors when props are empty', () => {
+      const isValid: boolean = SUT.validate({} as UserProps);
+      expect(isValid).toBeFalsy();
+      expect(SUT.errors!['password']).toStrictEqual(
+        [
+          'password should not be empty',
+          'password must be a string',
+          'password must be shorter than or equal to 100 characters'
+        ],
+      );
+    });
+
+    it('should return error when password is an empty string', () => {
+      const userProps = { ...userDateBuilder(), password: "" };
+      const isValid: boolean = SUT.validate(userProps);
+      expect(isValid).toBeFalsy();
+      expect(SUT.errors).toStrictEqual({
+        password: ['password should not be empty']
+      });
+    });
+
+    it('should return errors when password is not a string', () => {
+      const userProps = { ...userDateBuilder(), password: 123 as any };
+      const isValid: boolean = SUT.validate(userProps);
+      expect(isValid).toBeFalsy();
+      expect(SUT.errors).toStrictEqual({
+        password: [
+          'password must be a string',
+          'password must be shorter than or equal to 100 characters'
+        ]
+      });
+    });
+
+    it('should return error when name exceeds 255 characters', () => {
+      const userProps = { ...userDateBuilder(), password: 'a'.repeat(256) };
+      const isValid: boolean = SUT.validate(userProps);
+      expect(isValid).toBeFalsy();
+      expect(SUT.errors).toStrictEqual({
+        password: ['password must be shorter than or equal to 100 characters']
+      });
+    });
+  });
+
+  describe('CreatedAt field validation', () => {
+
+    it('should return error when createdAt is not a Date', () => {
+      const userProps = { ...userDateBuilder(), createdAt: "" as any };
+      const isValid: boolean = SUT.validate(userProps);
+      expect(isValid).toBeFalsy();
+      expect(SUT.errors).toStrictEqual({
+        createdAt: ['createdAt must be a Date instance']
+      });
+    });
+  });
 });
