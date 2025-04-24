60684d47913574ea8fe02ad1fbeac967acadd32d
akihito7 <akihitopro7@gmail.com>
2025-04-24 16:56:42 -0300

    feat : test units applyPaginate method

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -102,4 +102,29 @@ describe('SearchableInMemoryRepository – applyFilter', () => {
     });
   })
 
+  describe('ApplyPaginate method', () => {
+    it('should paginate items', async () => {
+      SUT = new StubClassInMemory();
+      items = [
+        new StubEntity({ name: 'a', price: 50 }),
+        new StubEntity({ name: 'b', price: 50 }),
+        new StubEntity({ name: 'c', price: 50 }),
+        new StubEntity({ name: 'd', price: 50 }),
+        new StubEntity({ name: 'e', price: 50 }),
+      ];
+      
+      SUT.sortableFields = ['name']
+      let itemsPaginated = await SUT['applyPaginate'](items, 1, 2)
+      expect(itemsPaginated).toStrictEqual([items[0], items[1]])
+
+      itemsPaginated = await SUT['applyPaginate'](items, 2, 2)
+      expect(itemsPaginated).toStrictEqual([items[2], items[3]])
+
+      itemsPaginated = await SUT['applyPaginate'](items, 3, 2)
+      expect(itemsPaginated).toStrictEqual([items[4]])
+
+      itemsPaginated = await SUT['applyPaginate'](items, 4, 2)
+      expect(itemsPaginated).toStrictEqual([])
+    })
+  })
 });
