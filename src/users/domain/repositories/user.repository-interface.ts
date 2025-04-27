72c5f1b82b12dc13ce8873f353686af61cbdc823
akihito7 <akihitopro7@gmail.com>
2025-04-27 15:09:48 -0300

    refactor : user repository interface

--- src/users/domain/repositories/user.repository-interface.ts
+++ src/users/domain/repositories/user.repository-interface.ts
@@ -1,9 +1,24 @@
-import { InMemoryRepository } from "@/shared/domain/repositories/in-memory-repository";
 import { UserEntity } from "../entities/user.entity";
-import { SearchableInMemoryRepository } from "@/shared/domain/repositories/searchable-in-memory-repository";
-import { ISearchableRepository } from "@/shared/domain/repositories/searchable.interface";
+import { ISearchableRepository, SearchParams as DefaultSearchParams, SearchResult as DefaultSearchResult } from "@/shared/domain/repositories/searchable.interface";
 
-export interface IUserRepository extends ISearchableRepository<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity>;
-  emailExists(email: string): Promise<void>
+export namespace IUserRepository {
+
+  export type Filter = string;
+
+  export class SearchParams extends DefaultSearchParams<Filter> { }
+
+  export class SearchResult extends DefaultSearchResult<UserEntity, Filter> { }
+
+  export interface Repository
+    extends ISearchableRepository
+    <
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>;
+    emailExists(email: string): Promise<void>
+  }
 }
+
