6b87705e06e77ef758d3610e5ab4b6319aca695b
akihito7 <akihitopro7@gmail.com>
2025-04-28 14:51:17 -0300

    feat : implemented useCase signup part 2 and added class badRequestError

--- src/users/application/use-cases/signup.use-cases.ts
+++ src/users/application/use-cases/signup.use-cases.ts
@@ -1,3 +1,7 @@
+import { UserEntity } from "@/users/domain/entities/user.entity";
+import { IUserRepository } from "@/users/domain/repositories/user.repository-interface";
+import { BadRequestError } from "../errors/bad-request-error";
+
 export namespace SignupUseCase {
   interface Input {
     name: string;
@@ -13,8 +17,16 @@ export namespace SignupUseCase {
   }
 
   export class UseCase {
+    constructor(private readonly userRepository: IUserRepository.Repository) { }
     async execute(input: Input): Promise<Output> {
-      throw new Error("Method not implemented")
+      const { name, email, password } = input;
+      if (!name || !email || !password) {
+        throw new BadRequestError("Input data is not provided.")
+      }
+      await this.userRepository.emailExists(email);
+      const userEntity = new UserEntity({ email, name, password });
+      await this.userRepository.insert(userEntity)
+      return userEntity.toJson()
     }
   }
 }
