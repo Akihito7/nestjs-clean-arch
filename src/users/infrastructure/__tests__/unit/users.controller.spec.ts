ffacf908314f385ae29d0f05a3eddc6b6739d67b
akihito7 <akihitopro7@gmail.com>
2025-05-08 09:14:47 -0300

    feat : added unit tests user controller create

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -1,18 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { SignupUseCase } from '@/users/application/use-cases/signup.use-case';
+import { SignupDTO } from '../../dtos/signup.dto';
 import { UsersController } from '../../users.controller';
+import { UUIDTypes } from 'uuid';
 
-/* describe('UsersController', () => {
-  let controller: UsersController;
+describe('UsersController unit tests', () => {
+  let SUT: UsersController;
+  let id: UUIDTypes;
+  let output: SignupUseCase.Output;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+    SUT = new UsersController();
+    id = '8ca39979-630e-4939-afcf-64f1d52d29bb';
+    output = {
+      id,
+      email: 'test@gmail.com',
+      name: 'Jane Doe',
+      password: '1234',
+      createdAt: new Date()
+    }
 
-    controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(SUT).toBeDefined();
   });
-}); */
+
+  it('should create a user', async () => {
+
+    const input: SignupDTO = {
+      email: 'test@gmail.com',
+      name: 'Jane Doe',
+      password: '1234'
+    };
+
+    const mockSignupUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output))
+    };
+
+    SUT['signupUseCase'] = mockSignupUseCase as any
+
+    const result = await SUT.signup(input);
+
+    expect(result).toMatchObject(output);
+    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input);
+  })
+}); 
