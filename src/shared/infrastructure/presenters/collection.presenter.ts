27ae112c39aed2f96ee9ef693a0a97ace91d4818
akihito7 <akihitopro7@gmail.com>
2025-05-15 11:39:29 -0300

    feat : added class collection presenter

---
+++ src/shared/infrastructure/presenters/collection.presenter.ts
@@ -0,0 +1,20 @@
+import { Exclude, Expose } from "class-transformer";
+import { PaginationPresenter } from "./pagination.presenter";
+
+export abstract class CollectionPresenter {
+
+  @Exclude()
+  protected paginationPresenterProps: PaginationPresenter;
+
+  constructor(props: PaginationPresenter) {
+    this.paginationPresenterProps = props;
+  };
+
+  @Expose()
+  get meta() {
+    return this.paginationPresenterProps;
+  }
+
+  abstract get data()
+
+}
