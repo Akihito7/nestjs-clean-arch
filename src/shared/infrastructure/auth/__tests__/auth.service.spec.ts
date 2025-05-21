36819c2d1ce0ded3a0f9d0d02916d99871a2a32a
akihito7 <akihitopro7@gmail.com>
2025-05-21 12:08:32 -0300

    feat : added auth service unit tests

--- src/shared/infrastructure/auth/__tests__/auth.service.spec.ts
+++ src/shared/infrastructure/auth/__tests__/auth.service.spec.ts
@@ -1,18 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from '../auth.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "../auth.service";
+import { JwtModule } from "@nestjs/jwt";
+import { EnvConfigModule } from "../../env-config/env-config.module";
+import { ConfigModule } from "@nestjs/config";
+import { EnvConfigService } from "../../env-config/env-config.service";
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('AuthService unit tests', () => {
 
-  beforeEach(async () => {
+  let SUT: AuthService;
+
+  beforeAll(async () => {
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [JwtModule.registerAsync({
+        imports: [EnvConfigModule],
+        useFactory: async (configService: EnvConfigService) => ({
+          global: true,
+          secret: configService.getJwtSecret(),
+          signOptions: { expiresIn: configService.getExpiresInSeconds() },
+        }),
+        inject: [EnvConfigService],
+      }), EnvConfigModule, ConfigModule],
+      providers: [AuthService]
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    SUT = module.get<AuthService>(AuthService);
+
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it('should create a token jwt', async () => {
+    const { acessToken } = SUT.generateToken('fakeID');
+    expect(typeof acessToken).toEqual('string')
+    expect(acessToken).toBeDefined();
+  })
+
+  it('should verify token', async () => {
+    const { acessToken } = SUT.generateToken('fakeID');
+    const isValidToken = await SUT.verifyJwt(acessToken);
+    expect(isValidToken).toBeTruthy();
+  })
+
+})
